91ae73b96b0d0dc363f8d5da8a5e021d9e085501
Carl Mercier <carl@carlmercier.com>
2023-12-11 13:24:31 -0500

    Update for latest version

--- lgtv_init.lua
+++ lgtv_init.lua
@@ -1,5 +1,6 @@
 --
 -- Tested with LGWebOSRemote as of December 11, 2023. Make sure you're on the right version!
+-- See README for installation instructions.
 --
 
 local tv_input = "HDMI_1" -- Input to which your Mac is connected
@@ -14,12 +15,16 @@ local disable_lgtv = false
 local tv_name = "MyTV" -- Name of your TV, set when you run `lgtv auth`
 local connected_tv_identifiers = {"LG TV", "LG TV SSCR2"} -- Used to identify the TV when it's connected to this computer
 local screen_off_command = "off" -- use "screenOff" to keep the TV on, but turn off the screen.
-local lgtv_path = "~/opt/lgtv/bin/lgtv" -- Full path to lgtv executable
+local lgtv_path = "~/.local/bin/lgtv" -- Full path to lgtv executable
 local lgtv_cmd = lgtv_path.." --ssl --name "..tv_name
 local app_id = "com.webos.app."..tv_input:lower():gsub("_", "")
 
+function lgtv_log_d(message)
+  if debug then print(message) end
+end
+
 function lgtv_current_app_id()
-  local foreground_app_info = exec_command("getForegroundAppInfo")
+  local foreground_app_info = lgtv_exec_command("getForegroundAppInfo")
   for w in foreground_app_info:gmatch('%b{}') do
     if w:match('\"response\"') then
       local match = w:match('\"appId\"%s*:%s*\"([^\"]+)\"')
@@ -30,7 +35,7 @@ function lgtv_current_app_id()
   end
 end
 
-function tv_is_connected()
+function lgtv_is_connected()
   for i, v in ipairs(connected_tv_identifiers) do
     if hs.screen.find(v) ~= nil then
       return true
@@ -40,12 +45,12 @@ function tv_is_connected()
   return false
 end
 
-function dump_table(o)
+function lgtv_dump_table(o)
   if type(o) == 'table' then
     local s = '{ '
     for k,v in pairs(o) do
       if type(k) ~= 'number' then k = '"'..k..'"' end
-      s = s .. '['..k..'] = ' .. dump_table(v) .. ','
+      s = s .. '['..k..'] = ' .. lgtv_dump_table(v) .. ','
     end
     return s .. '} '
   else
@@ -53,13 +58,9 @@ function dump_table(o)
   end
 end
 
-function exec_command(command)
+function lgtv_exec_command(command)
   command = lgtv_cmd.." "..command
-
-  if debug then
-    print("Executing command: "..command)
-  end
-
+  lgtv_log_d("Executing command: "..command)
   return hs.execute(command)
 end
 
@@ -67,26 +68,24 @@ function lgtv_disabled()
   return file_exists("./disable_lgtv") or file_exists(os.getenv('HOME') .. "/.disable_lgtv")
 end
 
-if debug then
-  print ("TV name: "..tv_name)
-  print ("TV input: "..tv_input)
-  print ("LGTV path: "..lgtv_path)
-  print ("LGTV command: "..lgtv_cmd)
-  print ("App ID: "..app_id)
-  print("lgtv_disabled: "..tostring(lgtv_disabled()))
-  if not lgtv_disabled() then
-    print (exec_command("swInfo"))
-    print (exec_command("getForegroundAppInfo"))
-    print("Connected screens: "..dump_table(hs.screen.allScreens()))
-    print("TV is connected? "..tostring(tv_is_connected()))
-  end
+lgtv_log_d ("TV name: "..tv_name)
+lgtv_log_d ("TV input: "..tv_input)
+lgtv_log_d ("LGTV path: "..lgtv_path)
+lgtv_log_d ("LGTV command: "..lgtv_cmd)
+lgtv_log_d ("App ID: "..app_id)
+lgtv_log_d("lgtv_disabled: "..tostring(lgtv_disabled()))
+if not lgtv_disabled() then
+  lgtv_log_d (lgtv_exec_command("swInfo"))
+  lgtv_log_d (lgtv_exec_command("getForegroundAppInfo"))
+  lgtv_log_d("Connected screens: "..lgtv_dump_table(hs.screen.allScreens()))
+  lgtv_log_d("TV is connected? "..tostring(lgtv_is_connected()))
 end
 
 watcher = hs.caffeinate.watcher.new(function(eventType)
-  if debug then print("Received event: "..(eventType or "")) end
+  lgtv_log_d("Received event: "..(eventType or ""))
 
   if lgtv_disabled() then
-    if debug then print("LGTV feature disabled. Skipping.") end
+    lgtv_log_d("LGTV feature disabled. Skipping.")
     return
   end
 
@@ -94,28 +93,28 @@ watcher = hs.caffeinate.watcher.new(function(eventType)
       eventType == hs.caffeinate.watcher.systemDidWake or
       eventType == hs.caffeinate.watcher.screensDidUnlock) and not lgtv_disabled() then
 
-    exec_command("on") -- wake on lan
-    exec_command("screenOn") -- turn on screen
-    if debug then print("TV was turned on") end
+    lgtv_exec_command("on") -- wake on lan
+    lgtv_exec_command("screenOn") -- turn on screen
+    lgtv_log_d("TV was turned on")
 
     if lgtv_current_app_id() ~= app_id and switch_input_on_wake then
-      exec_command("startApp "..app_id)
-      if debug then print("TV input switched to "..app_id) end
+      lgtv_exec_command("startApp "..app_id)
+      lgtv_log_d("TV input switched to "..app_id)
     end
   end
 
-  if (tv_is_connected() and (eventType == hs.caffeinate.watcher.screensDidSleep or
+  if (lgtv_is_connected() and (eventType == hs.caffeinate.watcher.screensDidSleep or
       eventType == hs.caffeinate.watcher.systemWillPowerOff) and not lgtv_disabled()) then
 
     if lgtv_current_app_id() ~= app_id and prevent_sleep_when_using_other_input then
-      if debug then print("TV is currently on another input ("..lgtv_current_app_id().."). Skipping powering off.") end
+      lgtv_log_d("TV is currently on another input ("..lgtv_current_app_id().."). Skipping powering off.")
       return
     end
 
     -- This puts the TV in standby mode.
     -- For true "power off" use `off` instead of `screenOff`.
-    exec_command(screen_off_command)
-    if debug then print("TV screen was turned off with command `"..screen_off_command.."`.") end
+    lgtv_exec_command(screen_off_command)
+    lgtv_log_d("TV screen was turned off with command `"..screen_off_command.."`.")
   end
 end)
 watcher:start()
